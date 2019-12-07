cba82712d87e8d59cc07b17a9ec3da9cd1fb296f
ramya20-02 <thota.ramya91@gmail.com>
2019-12-07 15:29:46 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,31 @@
 // Include form validation functions here
+function validation() {
+                var a = document.forms["form"]["txtTitle"].value;
+                if(a == "" ) {
+                                alert("Title is Required");
+                                return false;
+                             }
+                if ((a.length < 2) || (a.length > 100)) {
+                                alert("Title should have 2 to 100 characters.");
+                                return false;
+                             }
+                var b = document.forms["form"]["txtGross"].value;
+                if(b == "" ) {
+                                alert("Box Office is required.");
+                                return false;
+                             }
+                if(/[^0-9/]/.test(b)) {
+                                alert("Box Office has to be a number.");
+                                return false;
+                             }
+                var c = document.forms["form"]["txtDoL"].value;
+                if( c == "" ){
+                                alert("Date of Launch is required.");
+                                return false;
+                             }
+				var d = document.forms["form"]["txtGenre"].value;
+                if( d == "" ){
+                                alert("Select one Genre.");
+                                return false;
+                             }
+                      }
